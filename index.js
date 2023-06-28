d9368d35b587a5c3e5881083cc7bafe5409e8f29
Tjips <thomas@scheepers.com>
2023-06-28 20:45:53 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+import express from "express"
+import fetch from "node-fetch"
+
+const app = express()
+
+const content = "/data/content.json" 
+
+// Route to index.ejs file
+app.get('/', (req, res) => {
+
+    fetchJson(content).then((data) => {
+        res.render('index', data)
+        console.log(data)
+    })
+});
+
+// Set up EJS as the view engine
+app.set('view engine', 'ejs')
+app.set("views", "./views")
+
+// Serve static files from the public directory
+app.use(express.static('public'))
+
+// Start the server
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+  console.log(`
+
+  ░██████╗███████╗██████╗░██╗░░░██╗███████╗██████╗░  ██████╗░███████╗░█████╗░██████╗░██╗░░░██╗
+  ██╔════╝██╔════╝██╔══██╗██║░░░██║██╔════╝██╔══██╗  ██╔══██╗██╔════╝██╔══██╗██╔══██╗╚██╗░██╔╝
+  ╚█████╗░█████╗░░██████╔╝╚██╗░██╔╝█████╗░░██████╔╝  ██████╔╝█████╗░░███████║██║░░██║░╚████╔╝░
+  ░╚═══██╗██╔══╝░░██╔══██╗░╚████╔╝░██╔══╝░░██╔══██╗  ██╔══██╗██╔══╝░░██╔══██║██║░░██║░░╚██╔╝░░
+  ██████╔╝███████╗██║░░██║░░╚██╔╝░░███████╗██║░░██║  ██║░░██║███████╗██║░░██║██████╔╝░░░██║░░░
+  ╚═════╝░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝  ╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═════╝░░░░╚═╝░░░
+
+  Application available on: http://localhost:${port}
+  `)
+});
+
+async function fetchJson(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+  
+  async function postJson(url, data) {
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    });
+    const responseData = await response.json();
+    return responseData;
+}
